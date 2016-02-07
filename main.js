1c780f067b13486a63ec0f72d473d4a959eccf4f
DevonMalling <devon.malling@gmail.com>
2016-02-07 09:08:20 -0700

    Fixed mobile menu and tablet menu

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ $(document).on('ready', function() {
 	jQuery(function($){
 		$('.hamburger-icon').click(function(){
 			$('.responsive-nav-menu').toggleClass('expand')
+			$('.nav-bar').toggleClass('expand')
+			$('.wrapper').toggleClass('scrollable')
+			$('.footer').toggleClass('no-footer')
 		})
 	})
+
+	// Toggles classes on expanded mobile nav
 	jQuery(function($){
 		$('.mobile-nav').click(function(){
 			$('.responsive-menu').toggleClass('expand')
+			$('.wrapper').toggleClass('scrollable')
+			$('.footer').toggleClass('no-footer')
 		})
 	})
 
